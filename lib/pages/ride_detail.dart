1800ac8815d23a9388a8187759f62a3816ba4397
namrapatel2511 <21it115@charusat.edu.in>
2023-10-04 22:20:32 +0530

    live location

--- lib/pages/ride_detail.dart
+++ lib/pages/ride_detail.dart
@@ -14,6 +14,37 @@ class DetailedRidePage extends StatefulWidget {
 
 class _DetailedRidePageState extends State<DetailedRidePage> {
   GoogleMapController? _controller;
+  Map<String, dynamic> liveLocationData = {};
+  bool isLoading = false;
+  LatLng? markerPosition;
+  String requestStatus = '';
+
+  @override
+  void initState() {
+    super.initState();
+
+    _fetchRequestStatus();
+  }
+
+  void _fetchRequestStatus() async {
+    try {
+      final rideRequestQuerySnapshot = await FirebaseFirestore.instance
+          .collection('riderequest')
+          .where('rideId', isEqualTo: widget.rideId)
+          .get();
+
+      if (rideRequestQuerySnapshot.docs.isNotEmpty) {
+        final rideRequestData =
+            rideRequestQuerySnapshot.docs.first.data() as Map<String, dynamic>;
+        final requestStatus = rideRequestData['status'] ?? '';
+        setState(() {
+          this.requestStatus = requestStatus;
+        });
+      }
+    } catch (e) {
+      print('Error fetching request status: $e');
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -21,68 +52,102 @@ class _DetailedRidePageState extends State<DetailedRidePage> {
       appBar: AppBar(
         title: Text('Detailed Ride'),
       ),
-      body: FutureBuilder<DocumentSnapshot>(
-        future: FirebaseFirestore.instance
-            .collection('rides')
-            .doc(widget.rideId)
-            .get(),
-        builder: (context, snapshot) {
-          if (snapshot.connectionState == ConnectionState.waiting) {
-            return Center(child: CircularProgressIndicator());
-          }
-
-          if (!snapshot.hasData) {
-            return Center(child: Text('Ride details not found.'));
-          }
-
-          final rideData = snapshot.data!.data() as Map<String, dynamic>;
-
-          final liveLocationData = rideData['liveLocation'];
-
-          return SingleChildScrollView(
-            padding: EdgeInsets.all(16),
-            child: Column(
-              children: [
-                Text('Status: ${widget.status}'),
-                SizedBox(height: 16),
-                Container(
-                  height: 300,
-                  child: GoogleMap(
-                    onMapCreated: (controller) {
-                      setState(() {
-                        _controller = controller;
-                      });
-                    },
-                    initialCameraPosition: CameraPosition(
-                      target: LatLng(0, 0), // Initial position
-                      zoom: 12,
-                    ),
-                    markers: _buildMarkers(liveLocationData),
+      body: SingleChildScrollView(
+        padding: EdgeInsets.all(16),
+        child: Column(
+          children: [
+            Text('Status: ${widget.status}'),
+            SizedBox(height: 16),
+            if (requestStatus == 'accepted')
+              Container(
+                height: 300,
+                child: GoogleMap(
+                  onMapCreated: (controller) {
+                    setState(() {
+                      _controller = controller;
+                    });
+                  },
+                  initialCameraPosition: CameraPosition(
+                    target: LatLng(0, 0), // Initial position
+                    zoom: 12,
                   ),
+                  markers: _buildMarkers(),
                 ),
-              ],
-            ),
-          );
-        },
+              ),
+            SizedBox(height: 16),
+            if (requestStatus == 'accepted')
+              ElevatedButton(
+                onPressed: () {
+                  _fetchLiveLocationData();
+                },
+                child: isLoading
+                    ? CircularProgressIndicator()
+                    : Text('Live Location'),
+              ),
+          ],
+        ),
       ),
     );
   }
 
-  // Build map markers from live location data
-  Set<Marker> _buildMarkers(Map<String, dynamic> liveLocationData) {
-    // Implement logic to convert live location data to markers
-    // Example:
+  Set<Marker> _buildMarkers() {
     final markers = <Marker>{};
-    liveLocationData.forEach((userId, location) {
-      final lat = location['latitude'] as double;
-      final lng = location['longitude'] as double;
+    if (markerPosition != null) {
       final marker = Marker(
-        markerId: MarkerId(userId),
-        position: LatLng(lat, lng),
-        // Add other marker properties as needed
+        markerId: MarkerId('userid'),
+        position: markerPosition!,
+        icon: BitmapDescriptor.defaultMarkerWithHue(BitmapDescriptor.hueBlue),
       );
       markers.add(marker);
-    });
+    }
     return markers;
+  }
+
+  void _fetchLiveLocationData() async {
+    try {
+      final snapshot = await FirebaseFirestore.instance
+          .collection('ride_navigation')
+          .doc(widget.rideId)
+          .get();
+
+      if (snapshot.exists) {
+        print('Live location data retrieved: ${snapshot.data()}');
+        final data = snapshot.data() as Map<String, dynamic>;
+        setState(() {
+          liveLocationData = data;
+          final latitude = data['latitude'] as double;
+          final longitude = data['longitude'] as double;
+          markerPosition = LatLng(latitude, longitude);
+        });
+        print('Live location data updated: $liveLocationData');
+        _moveCameraToMarker();
+      } else {
+        ScaffoldMessenger.of(context).showSnackBar(
+          SnackBar(
+            content: Text('Live location data not found.'),
+          ),
+        );
+      }
+    } catch (e) {
+      print('Error fetching live location data: $e');
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(
+          content: Text('Error fetching live location data.'),
+        ),
+      );
+    }
+  }
+
+  void _moveCameraToMarker() {
+    if (_controller != null && markerPosition != null) {
+      _controller!.animateCamera(
+        CameraUpdate.newCameraPosition(
+          CameraPosition(
+            target: markerPosition!,
+            zoom: 15,
+          ),
+        ),
+      );
+    }
   }
 }
